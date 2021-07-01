44c71623ff4949d4a44da9f470c7759470337527
kebinh1235 <kebinh1235@gmail.com>
2021-07-01 21:00:29 +0700

    send contact

--- services/Service.js
+++ services/Service.js
@@ -1,7 +1,9 @@
 import AuthService from './AuthService';
 import CategoryService from './CategoryService';
+import ContactService from './ContactService';
 
 export default ($axios) => ({
     Auth: AuthService($axios),
     Category: CategoryService($axios),
+    Contact: ContactService($axios),
 })
